ded0b0c40352bbf705d6d717dd3d9bd3450fa21a
forw39 <forw39@gmail.com>
2020-08-19 14:49:08 +0200

    condition changed

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './assets/styles/main.css'
-import './assets/styles/main.scss'
-import './assets/js/main.js'
-import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
-import '@simonwep/pickr/dist/themes/classic.min.css';   // 'classic' theme
-import '@simonwep/pickr/dist/themes/monolith.min.css';  // 'monolith' theme
-import '@simonwep/pickr/dist/themes/nano.min.css';      // 'nano' theme
-// import Pickr from '@simonwep/pickr';
-import Pickr from '@simonwep/pickr/dist/pickr.es5.min'
